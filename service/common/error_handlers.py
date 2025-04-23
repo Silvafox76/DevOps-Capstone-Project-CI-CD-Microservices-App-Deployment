cbebd8db4d532a21a6a4cd3d4e1b31f24f070378
Silvafox76 <ryan.w.dear@gmail.com>
2025-04-23 09:18:20 -0400

    Fix lint errors in error_handlers.py

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -6,6 +6,7 @@ from service.models import DataValidationError
 from service import app
 from . import status
 
+
 ######################################################################
 # Error Handlers
 ######################################################################
@@ -14,6 +15,7 @@ from . import status
 def request_validation_error(error):
     """Handles Value Errors from bad data"""
     return bad_request(error)
+
 
 @app.errorhandler(status.HTTP_400_BAD_REQUEST)
 def bad_request(error):
@@ -24,10 +26,11 @@ def bad_request(error):
         jsonify(
             status=status.HTTP_400_BAD_REQUEST,
             error="Bad Request",
-            message=message
+            message=message,
         ),
         status.HTTP_400_BAD_REQUEST,
     )
+
 
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
 def not_found(error):
@@ -38,24 +41,26 @@ def not_found(error):
         jsonify(
             status=status.HTTP_404_NOT_FOUND,
             error="Not Found",
-            message=message
+            message=message,
         ),
         status.HTTP_404_NOT_FOUND,
     )
 
+
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
 def method_not_supported(error):
-    """Handles unsupported HTTP methods with 405_METHOD_NOT_ALLOWED"""
+    """Handles unsupported HTTP methods with 405_METHOD_NOT_SUPPORTED"""
     message = str(error)
     app.logger.warning(message)
     return (
         jsonify(
             status=status.HTTP_405_METHOD_NOT_ALLOWED,
-            error="Method Not Allowed",
-            message=message
+            error="Method not Allowed",
+            message=message,
         ),
         status.HTTP_405_METHOD_NOT_ALLOWED,
     )
+
 
 @app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 def mediatype_not_supported(error):
@@ -65,22 +70,19 @@ def mediatype_not_supported(error):
     return (
         jsonify(
             status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            error="Unsupported Media Type",
-            message=message
+            error="Unsupported media type",
+            message=message,
         ),
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
     )
 
+
 @app.errorhandler(500)
 def internal_server_error(error):
-    """Handles unexpected server errors with 500_INTERNAL_SERVER_ERROR"""
+    """Handles unexpected server errors"""
     message = str(error)
     app.logger.error("Internal Server Error: %s", message)
     return (
-        jsonify(
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            error="Internal Server Error",
-            message=message
-        ),
-        status.HTTP_500_INTERNAL_SERVER_ERROR,
+        jsonify(status=500, error="Internal Server Error", message=message),
+        500,
     )
